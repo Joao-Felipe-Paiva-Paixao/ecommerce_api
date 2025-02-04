e0120c9097fb9d3fd05cb9c2cbf208974f2b6d8a
Joao-Felipe-Paiva-Paixao <jfera2007.jfpp@gmail.com>
2025-02-04 15:42:54 -0300

    Implementando trycatch
    Implementando trycatch nos métodos http

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -9,7 +9,8 @@ type User = {
 
 export class UsersController {
     static async getAll(req: Request, res: Response) {
-        const snapshot = await getFirestore().collection("users").get();
+        try {
+            const snapshot = await getFirestore().collection("users").get();
         const users = snapshot.docs.map(doc =>{
             return {
                 id: doc.id,
@@ -17,25 +18,46 @@ export class UsersController {
             };
         });
         res.send(users);
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro interno do Servidor"
+            })
+        };
     };
     static async getById(req: Request, res: Response) {
-        let  userId = (req.params.id);
+        
+        try {
+            let  userId = (req.params.id);
         const doc = await getFirestore().collection("users").doc(userId).get();
         res.send({
             id: doc.id,
             ...doc.data()
         });
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro interno do Servidor"
+            })
+        };
     };
     static async save(req: Request, res: Response) {
-        let user = req.body;
+        
+        try {
+            let user = req.body;
         const userSalvo = await getFirestore().collection("users").add(user);
         res.status(201).send({
             message: `Usuário ${userSalvo.id} criado com sucesso!`
         });
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro interno do Servidor"
+            })
+        };
     };
 
     static update(req: Request, res: Response) {
-        let userId = req.params.id;
+        
+        try {
+            let userId = req.params.id;
         let user = req.body as User;
 
         getFirestore().collection("users").doc(userId).set({
@@ -45,12 +67,25 @@ export class UsersController {
         res.send({
             message: "Usuário alterado com sucesso!"
         });
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro interno do Servidor"
+            })
+        };
     };
     static async delete(req:Request, res:Response) {
-        let userId = (req.params.id);
+        
+    try {
+         let userId = (req.params.id);
 
         await getFirestore().collection("users").doc(userId).delete();
 
         res.status(204).end();
+    }   
+     catch (error) {
+        res.status(500).send({
+            message: "Erro interno do Servidor"
+        })
     };
 };
+}
