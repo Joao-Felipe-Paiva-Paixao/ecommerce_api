2d7c012d295996a6e9e1d7863ee2bffdc1694cf2
Joao-Felipe-Paiva-Paixao <jfera2007.jfpp@gmail.com>
2025-02-10 21:24:10 -0300

    Implementação do celebrate no PUT
    Configuração do PUT e formatação do modelo de usuário

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,19 +1,13 @@
 import express from "express";
 import { UsersController } from "../controllers/users.controller";
 import asyncHandler from "express-async-handler";
-import { celebrate, Joi, Segments } from "celebrate";
+import { celebrate, Segments } from "celebrate";
+import { userSchema } from "../models/user.model";
 
 export const userRoutes = express.Router();
 
-let celebrateParams = {
-    [Segments.BODY]: Joi.object().keys({
-        nome: Joi.string().required(),
-        email: Joi.string().email().required()
-    })
-}
-
 userRoutes.get("/users", asyncHandler(UsersController.getAll));
 userRoutes.get("/users/:id", asyncHandler(UsersController.getById));
-userRoutes.post("/users", celebrate(celebrateParams), asyncHandler(UsersController.save));
-userRoutes.put("/users/:id", asyncHandler(UsersController.update));
+userRoutes.post("/users", celebrate({ [Segments.BODY]: userSchema }), asyncHandler(UsersController.save));
+userRoutes.put("/users/:id", celebrate({ [Segments.BODY]: userSchema }), asyncHandler(UsersController.update));
 userRoutes.delete("/users/:id", asyncHandler(UsersController.delete));
