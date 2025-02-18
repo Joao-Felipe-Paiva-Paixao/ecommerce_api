7883372d219625969cce34b0df6807a70bf91f71
Joao-Felipe-Paiva-Paixao <jfera2007.jfpp@gmail.com>
2025-02-18 16:37:01 -0300

    email.already.exists.error update
    Tratamento de erros pra usuários duplicados, formatação do error-handler.midlleware.ts

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,16 +1,13 @@
 import express,{Request, Response, NextFunction} from "express";
-import { ValidatonError } from "../errors/validation.error";
 import { InternalServerError } from "../errors/internal-server.error";
-import { NotFoundError } from "../errors/not-found.error";
 import { errors } from "celebrate";
+import { ErrorBase } from "../errors/base.error";
 
 export const errorHandler = (app: express.Express) => {
     app.use(errors());
     app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
         console.log(error)
-        if (error instanceof ValidatonError) {
-            error.send(res);
-        } else if (error instanceof NotFoundError) {
+        if (error instanceof ErrorBase) {
             error.send(res);
         } else {
             new InternalServerError().send(res);
