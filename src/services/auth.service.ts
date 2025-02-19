c3dbf6181dfd6ad990600995ee0ac44eb4d271cc
Joao-Felipe-Paiva-Paixao <jfera2007.jfpp@gmail.com>
2025-02-19 19:46:37 -0300

    Update auth.service.ts

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { EmailAlreadyExistsError } from "../errors/email.already.exists.error";
 import { User } from "../models/user.model";
-import { getAuth, UserRecord } from "firebase-admin/auth";
+import { FirebaseAuthError, getAuth, UserRecord } from "firebase-admin/auth";
+import { getAuth as getFirebaseAuth, signInWithEmailAndPassword, UserCredential } from "firebase/auth";
 
 export class AuthService {
     async create(user: User): Promise<UserRecord> {
@@ -9,10 +10,14 @@ export class AuthService {
             password: user.password,
             displayName: user.nome,
         }).catch(err => {
-            if (err.code === "auth/email-already-exists") {
+            if (err instanceof FirebaseAuthError && err.code === "auth/email-already-exists") {
                 throw new EmailAlreadyExistsError();
             }
             throw err;
         });
+    }
+
+    login(email: string, password: string): Promise<UserCredential> {
+        return signInWithEmailAndPassword(getFirebaseAuth(), email, password);
     }
 }
