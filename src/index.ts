f2170b005da9b305898ce704ecf76782b15ce0ca
Joao-Felipe-Paiva-Paixao <jfera2007.jfpp@gmail.com>
2025-02-24 14:10:05 -0300

    Validação de token de usuário

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { initializeApp } from 'firebase-admin/app';
-import { initializeApp as initializeFirebaseApp } from "firebase/app"; 
+import { initializeApp as initializeFirebaseApp } from "firebase/app";
 import { routes } from "./routes/index";
 import { errorHandler } from "./middlewares/error-handler.middleware";
 import { pageNotFoundHandler } from "./middlewares/page-not-found.middleware";
